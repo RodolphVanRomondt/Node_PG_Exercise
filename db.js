bb3be781ec86411b3f63d055cdb488c91d1cbf4e
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-03-23 12:22:14 -0700

    step 1: add a DB

--- db.js
+++ db.js
@@ -1,2 +1,20 @@
 /** Database setup for BizTime. */
 
+const pg = require("pg");
+
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = "postgresql:///biztime_test";
+} else {
+    DB_URI = "postgresql:///biztime";
+}
+
+let db = new pg.Client({
+    connectionString: DB_URI
+});
+
+db.connect()
+
+
+module.exports = db;
